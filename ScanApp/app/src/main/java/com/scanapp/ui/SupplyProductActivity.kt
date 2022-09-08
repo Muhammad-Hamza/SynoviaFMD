63ec41e7f7a2ce99c46905e4160e77435ceb35bb
Muhammad Asher Ansari <asher.ansari@nhu.edu.pk>
2022-09-08 15:25:46 +0500

    Fix redirecting issue as well as done passing data

--- ScanApp/app/src/main/java/com/scanapp/ui/SupplyProductActivity.kt
+++ ScanApp/app/src/main/java/com/scanapp/ui/SupplyProductActivity.kt
@@ -1,14 +1,45 @@
 package com.scanapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.scanapp.R
+import com.scanapp.ui.dashboard.ScannerActivity
 
-class SupplyProductActivity : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+
+class SupplyProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_supply_product)
+
+        val btnScanpak = findViewById<AppCompatButton>(R.id.btnScanpak)
+        btnScanpak.setOnClickListener {
+            val intent = Intent(this, ScannerActivity::class.java)
+            someActivityResultLauncher.launch(intent)
+        }
     }
+
+    // You can do the assignment inside onAttach or onCreate, i.e, before the activity is displayed
+    var someActivityResultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult> { result: ActivityResult ->
+            if (result.resultCode === RESULT_OK) {
+                // There are no request codes
+                val data: Intent = result.getData()!!
+                val resultData = data.getStringExtra("result")
+                if (!TextUtils.isEmpty(resultData)) {
+                    Toast.makeText(this, resultData, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Data not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
 }
--- ScanApp/app/src/main/java/com/scanapp/ui/dashboard/ScannerActivity.kt
+++ ScanApp/app/src/main/java/com/scanapp/ui/dashboard/ScannerActivity.kt
@@ -1,26 +1,30 @@
 package com.scanapp.ui.dashboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.CodeScannerView
 import com.budiyev.android.codescanner.DecodeCallback
 import com.scanapp.R
 
-class ScannerActivity : AppCompatActivity()
-{
+
+class ScannerActivity : AppCompatActivity() {
     private lateinit var codeScanner: CodeScanner
 
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner)
         val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
         codeScanner = CodeScanner(this, scannerView)
         codeScanner.decodeCallback = DecodeCallback {
             runOnUiThread {
-                Toast.makeText(this, it.text, Toast.LENGTH_LONG).show()
+//                Log.e("asd",it.text)
+//                Toast.makeText(this, it.text, Toast.LENGTH_LONG).show()
+                val returnIntent = Intent()
+                returnIntent.putExtra("result", it.text)
+                setResult(RESULT_OK, returnIntent)
+                finish()
             }
         }
         scannerView.setOnClickListener {
@@ -32,6 +36,10 @@ class ScannerActivity : AppCompatActivity()
         super.onResume()
         codeScanner.startPreview()
     }
+    //2022-09-08 00:04:25.162 10561-10561/com.scanapp E/asd: 01022564353848351725123110a0001210000000001
+    //2022-09-08 00:07:06.826 10561-10561/com.scanapp E/asd: 0102256435384835172502091000001210000000003
+    //2022-09-08 00:07:49.099 10561-10561/com.scanapp E/asd: 01022564353848351725020910000012100000000031
+    //2022-09-08 00:08:30.567 10561-10561/com.scanapp E/asd: 0102256435384835172502091000099210000000011
 
     override fun onPause() {
         codeScanner.releaseResources()
